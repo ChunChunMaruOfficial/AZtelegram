dc1bdba60280a452c354c62942c0475f1deacfac
KANTNOLI <zahargysev02@gmail.com>
2024-06-12 18:48:19 +0300

    Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Work</h1>} />
+      <Route path="*" element={<h1>ERROR 404</h1>} />
+      {/* {path="*"} Вывод ошибки, если пути не существует */}
+    </Routes>
   );
 }
 
