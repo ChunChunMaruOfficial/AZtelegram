29be42027a0531de2502f01b2c5f8578299eb59f
KANTNOLI <zahargysev02@gmail.com>
2024-06-17 02:06:58 +0300

    little update

--- frontend/src/types/ChatListTypes.tsx
+++ frontend/src/types/ChatListTypes.tsx
@@ -1,14 +1,12 @@
-//Регул отображение поиска чатов и текста
 export type ChatListVanish = {
   display?: string;
 };
-
-//Регул отображение кнопки меню
 export type BurgerPosition = {
   justifyContent?: string;
 };
-
-//Регул отображение списка чатов
 export type SetHandleChats = {
   height?: string;
+};
+export type SetMargin = {
+  marginLeft?: string;
 };
